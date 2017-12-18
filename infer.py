b652cdd8cf2d244e2780a326a03ee400ab3bbf4a
junli <junli.westwell-lab.com>
2017-12-18 17:31:47 +0800

    path for caffe

--- infer.py
+++ infer.py
@@ -1,10 +1,12 @@
 import numpy as np
 from PIL import Image
 
+import _init_paths
+
 import caffe
 
 # load image, switch to BGR, subtract mean, and make dims C x H x W for Caffe
-im = Image.open('pascal/VOC2010/JPEGImages/2007_000129.jpg')
+im = Image.open('data/pascal/VOC2012/JPEGImages/2007_000129.jpg')
 in_ = np.array(im, dtype=np.float32)
 in_ = in_[:,:,::-1]
 in_ -= np.array((104.00698793,116.66876762,122.67891434))
@@ -18,3 +20,36 @@ net.blobs['data'].data[...] = in_
 # run net and take argmax for prediction
 net.forward()
 out = net.blobs['score'].data[0].argmax(axis=0)
+
+#------------------------------------------
+arr=out.astype(np.uint8)
+im=Image.fromarray(arr)
+
+palette=[]
+for i in range(256):
+    palette.extend((i,i,i))
+palette[:3*21]=np.array([[0, 0, 0],
+                            [128, 0, 0],
+                            [0, 128, 0],
+                            [128, 128, 0],
+                            [0, 0, 128],
+                            [128, 0, 128],
+                            [0, 128, 128],
+                            [128, 128, 128],
+                            [64, 0, 0],
+                            [192, 0, 0],
+                            [64, 128, 0],
+                            [192, 128, 0],
+                            [64, 0, 128],
+                            [192, 0, 128],
+                            [64, 128, 128],
+                            [192, 128, 128],
+                            [0, 64, 0],
+                            [128, 64, 0],
+                            [0, 192, 0],
+                            [128, 192, 0],
+                            [0, 64, 128]], dtype='uint8').flatten()
+
+im.putpalette(palette)
+im.show()
+im.save('test.png')
